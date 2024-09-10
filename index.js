7c5f44761a513b3963e3d75cc9e786427dbe07cc
elias12vr <yoshytony0@gmail.com>
2024-09-10 14:29:54 -0600

    Creamos una carpeta para hacer una conexion con la base de dato

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express();
+
+app.use(express.static(__dirname + '/public/'))
+app.listen('3000', function(){
+    console.log('Servidor funcionando en el puerto 3000')
+});
